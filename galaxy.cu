41a8ed8d41e10f90ffea9600855c6692591e9b63
carinorthrop <50886022+carinorthrop@users.noreply.github.com>
2020-11-16 11:53:46 -0500

    initial commit

---
+++ galaxy.cu
@@ -0,0 +1,39 @@
+/* 
+N-body simulation of a galaxy 
+Authors: JT Salisbury, Sydney O'Connor, Kyle Bush, Caroline Northrop 
+Parallel Computing 6068
+*/
+
+//TODO
+// parse csv file 
+void parse_input(){
+
+}
+
+//TODO 
+// physics helper functions
+void compute_force(){
+
+}
+
+void compute_acceleration(){
+
+}
+
+void compute_distance()
+{
+
+}
+
+void compute_next_pos(){
+    
+}
+
+// TODO 
+// animation stuff
+
+// TODO
+// main function to perform physic operations 
+int main(int argc, char* argv[]){
+    return 0;
+}
